ff63b86a7958e63ca55581f460a26a1006321025
Srabo93 <a.s-dev@web.de>
2021-11-14 14:23:38 +0100

    -

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -6,19 +6,21 @@ import useStorage from "../hooks/useFirebase/useStorage";
 
 const ProgressBar = ({ file, setFile }) => {
   const { url, progress } = useStorage(file);
+
   useEffect(() => {
     if (url) {
       setFile(null);
     }
   }, [url, setFile]);
+
   return (
-    <div>
+    <React.Fragment>
       <div
         className={classes.progress_bar}
         style={{ width: progress + "%" }}
       ></div>
       <FontAwesomeIcon icon={faUpload} className={classes.uploadIcon} />
-    </div>
+    </React.Fragment>
   );
 };
 
