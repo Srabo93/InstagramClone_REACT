aa4b06402b71d89457756f2f33f93950d8d909c8
Srabo93 <a.s-dev@web.de>
2021-11-14 12:24:44 +0100

    -

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhotoVideo } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
 
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
+      <h1>
+        ClonesterGram <FontAwesomeIcon icon={faPhotoVideo} />
+      </h1>
       <h2>Your Pictures</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
-  )
-}
+  );
+};
 
 export default Title;
