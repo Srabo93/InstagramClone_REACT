10c3585e81a2e2edaeb977fb92b945bb884cc60d
Srabo93 <A.S-dev@web.de>
2021-12-03 17:36:04 +0100

    minor cleanup no tused modules

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,36 +1,13 @@
 import React from "react";
 import classes from "./Modal.module.css";
-//FireStore
-import { doc, deleteDoc } from "firebase/firestore";
-import { db } from "../firebase/config";
-//Storage
-import { getStorage, ref, deleteObject } from "firebase/storage";
 import { motion } from "framer-motion";
 import InteractionBar from "./InteractionBar";
-import Wrapper from "../UI/Wrapper";
 
 const Modal = ({ displayImg, onCloseBackdrop }) => {
   const onClickBackdrop = (event) => {
     if (event.target.classList.contains("Modal_backdrop__1DLHX")) {
       onCloseBackdrop(null);
     }
-  };
-
-  const deleteHanlder = () => {
-    deleteDoc(doc(db, "images", displayImg.id));
-
-    const storage = getStorage();
-
-    const desertRef = ref(storage, displayImg.url);
-
-    deleteObject(desertRef)
-      .then(() => {
-        console.log("storage deletion success");
-      })
-      .catch((error) => {
-        console.log("deletion failed");
-      });
-    onCloseBackdrop(null);
   };
 
   return (
