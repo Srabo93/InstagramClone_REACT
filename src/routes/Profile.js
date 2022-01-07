8c05d5cbce11a991bbcfa3798c92ecaad7b26bf8
Srabo93 <A.S-dev@web.de>
2022-01-07 21:16:28 +0100

    added content to Profile route

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,31 +1,44 @@
 import React from "react";
-
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { Avatar, Container } from "@mui/material";
 const Profile = () => {
   return (
-    <div>
-      This is Profile SiteThis is Profile SiteThis is Profile SiteThis is
-      Profile SiteThis is Profile SiteThis is Profile SiteThis is Profile
-      SiteThis is Profile SiteThis is Profile SiteThis is Profile SiteThis is
-      Profile SiteThis is Profile SiteThis is Profile SiteThis is Profile
-      SiteThis is Profile SiteThis is Profile SiteThis is Profile SiteThis is
-      Profile SiteThis is Profile SiteThis is Profile SiteThis is Profile
-      SiteThis is Profile SiteThis is Profile SiteThis is Profile SiteThis is
-      Profile SiteThis is Profile SiteThis is Profile SiteThis is Profile
-      SiteThis is Profile SiteThis is Profile SiteThis is Profile SiteThis is
-      Profile SiteThis is Profile SiteThis is Profile SiteThis is Profile
-      SiteThis is Profile SiteThis is Profile SiteThis is Profile SiteThis is
-      Profile SiteThis is Profile SiteThis is Profile SiteThis is Profile
-      SiteThis is Profile SiteThis is Profile SiteThis is Profile SiteThis is
-      Profile SiteThis is Profile SiteThis is Profile SiteThis is Profile
-      SiteThis is Profile SiteThis is Profile SiteThis is Profile SiteThis is
-      Profile SiteThis is Profile SiteThis is Profile SiteThis is Profile
-      SiteThis is Profile SiteThis is Profile SiteThis is Profile SiteThis is
-      Profile SiteThis is Profile SiteThis is Profile SiteThis is Profile
-      SiteThis is Profile SiteThis is Profile SiteThis is Profile SiteThis is
-      Profile SiteThis is Profile SiteThis is Profile SiteThis is Profile
-      SiteThis is Profile SiteThis is Profile SiteThis is Profile SiteThis is
-      Profile Site
-    </div>
+    <Container
+      maxWidth="md"
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 10,
+      }}
+    >
+      <Card sx={{ maxWidth: "sm", padding: 3, textAlign: "center" }}>
+        <Avatar
+          alt="Random User"
+          src="https://source.unsplash.com/random"
+          sx={{
+            width: 240,
+            height: 240,
+            margin: "0 auto",
+          }}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            UserName
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Description about the user Description about the user Description
+            about the user Description about the user Description about the user
+            Description about the user Description about the user Description
+            about the user Description about the user Description about the user
+            Description about the user Description about the user Description
+            about the user
+          </Typography>
+        </CardContent>
+      </Card>
+    </Container>
   );
 };
 
