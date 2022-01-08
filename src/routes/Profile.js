4218b4d9b625239822425b7e8d4349ecee78c544
Srabo93 <A.S-dev@web.de>
2022-01-08 12:33:37 +0100

    added Content to Login.js

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -3,6 +3,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { Avatar, Container } from "@mui/material";
+
 const Profile = () => {
   return (
     <Container
