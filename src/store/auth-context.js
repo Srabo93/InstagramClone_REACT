3a083c82d66af3513ca441a2a1a63b4c3ab5d38f
Srabo93 <A.S-dev@web.de>
2022-01-13 16:27:59 +0100

    added UserId to context

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const AuthContext = React.createContext({
   user: "",
+  userId: "",
   accessToken: "",
   isLoggedIn: false,
   login: (accessToken) => {},
@@ -12,22 +13,26 @@ export const AuthContextProvider = (props) => {
   const initialToken = localStorage.getItem("token");
   const [accessToken, setAccessToken] = useState(initialToken);
   const [user, setUser] = useState(null);
+  const [userId, setUserId] = useState(null);
   const userIsLoggedIn = !!accessToken;
 
-  const loginHandler = (token, email) => {
+  const loginHandler = (token, email, uid) => {
     setAccessToken(token);
     localStorage.setItem("token", token);
     const name = email.split("@")[0];
     setUser(name);
+    setUserId(uid);
   };
   const logoutHandler = () => {
     setAccessToken(null);
     setUser(null);
+    setUserId(null);
     localStorage.removeItem("token");
   };
 
   const ContextValue = {
     user: user,
+    userId: userId,
     accessToken: accessToken,
     isLoggedIn: userIsLoggedIn,
     login: loginHandler,
