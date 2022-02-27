82b7ada1d2990c9513fc56b83e98face10e251b7
Srabo93 <A.S-dev@web.de>
2022-02-27 15:10:39 +0100

    -

--- src/components/UploadImgGrid.js
+++ src/components/UploadImgGrid.js
@@ -1,32 +1,12 @@
 import React from "react";
-import { db } from "../API/firebase";
 import { useAuth } from "../Auth/AuthContext";
 import useFirestore from "../hooks/useFirestore";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
-import ImageListItemBar from "@mui/material/ImageListItemBar";
-import IconButton from "@mui/material/IconButton";
-import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-import { doc, deleteDoc, where } from "firebase/firestore";
 
 const UploadImgGrid = () => {
   const { currentUser } = useAuth();
   const { docs } = useFirestore(`Users`, currentUser.email);
-
-  const deleteHandler = async (event) => {
-    console.log(event.target.value);
-    // try {
-    //   await deleteDoc(
-    //     doc(
-    //       db,
-    //       `Users/7fIuUFOGmJtFjuZHwdKw`,
-    //       where("url", "==", event.target.id)
-    //     )
-    //   );
-    // } catch (error) {
-    //   console.log(error);
-    // }
-  };
 
   return (
     <ImageList
@@ -43,19 +23,6 @@ const UploadImgGrid = () => {
             alt="randomimg"
             loading="lazy"
             sx={{ pt: 1 }}
-          />
-          <ImageListItemBar
-            position="top"
-            actionIcon={
-              <IconButton
-                color="primary"
-                onClick={deleteHandler}
-                value={doc.url}
-              >
-                <DeleteForeverIcon fontSize="large" />
-              </IconButton>
-            }
-            actionPosition="left"
           />
         </ImageListItem>
       ))}
