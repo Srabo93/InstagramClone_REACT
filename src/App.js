98a90c2617c4d7e8c5f75bfd23e132d550bc4de0
Srabo93 <A.S-dev@web.de>
2022-03-20 21:04:39 +0100

    added lazyload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import { Container } from "@mui/material";
@@ -6,60 +6,60 @@ import { ThemeProvider } from "@mui/material/styles";
 import { themeOptions } from "./components/UI/themeContext";
 import Home from "./components/routes/Home";
 import Navigation from "./components/Navigation";
-import Profile from "./components/routes/Profile";
-import Favorites from "./components/routes/Favorites";
-import Uploads from "./components/routes/Uploads";
 import Login from "./components/routes/Login";
 import ForgotPassword from "./components/routes/ForgotPassword";
 import ProfileUpdate from "./components/ProfileUpdate";
+import CircularProgress from "@mui/material/CircularProgress";
 
+const Profile = React.lazy(() => import("./components/routes/Profile"));
+const Favorites = React.lazy(() => import("./components/routes/Favorites"));
+const Uploads = React.lazy(() => import("./components/routes/Uploads"));
 function App() {
   return (
     <ThemeProvider theme={themeOptions}>
       <Container maxWidth="md" sx={{ mt: 10 }}>
-        <Routes>
-          <Route path="/" element={<Navigation />}>
-            <Route index element={<Home />} />
-            <Route
-              path="profile"
-              element={
-                <PrivateRoute>
-                  <Profile />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="profile/updateprofile"
-              element={
-                <PrivateRoute>
-                  <ProfileUpdate />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="favorites"
-              element={
-                <PrivateRoute>
-                  <Favorites />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="uploads"
-              element={
-                <PrivateRoute>
-                  <Uploads />
-                </PrivateRoute>
-              }
-            />
-            <Route path="test" element={<div>test</div>}>
-              <Route path="inside" element={<div>lol</div>} />
+        <Suspense fallback={<CircularProgress />}>
+          <Routes>
+            <Route path="/" element={<Navigation />}>
+              <Route index element={<Home />} />
+              <Route
+                path="profile"
+                element={
+                  <PrivateRoute>
+                    <Profile />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="profile/updateprofile"
+                element={
+                  <PrivateRoute>
+                    <ProfileUpdate />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="favorites"
+                element={
+                  <PrivateRoute>
+                    <Favorites />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="uploads"
+                element={
+                  <PrivateRoute>
+                    <Uploads />
+                  </PrivateRoute>
+                }
+              />
+              <Route path="login" element={<Login />} />
+              <Route path="forgot-password" element={<ForgotPassword />} />
+              <Route path="*" element={<Home />} />
             </Route>
-            <Route path="login" element={<Login />} />
-            <Route path="forgot-password" element={<ForgotPassword />} />
-            <Route path="*" element={<Home />} />
-          </Route>
-        </Routes>
+          </Routes>
+        </Suspense>
       </Container>
     </ThemeProvider>
   );
