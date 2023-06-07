5aa709f1ca249cc0082404caf8a99fa2b216dc6b
srabo93 <a.s-dev@web.de>
2023-06-08 01:34:46 +0200

    added error page

---
+++ src/components/ErrorPage.tsx
@@ -0,0 +1,61 @@
+import { Box, Typography } from "@mui/material";
+import { ErrorResponse } from "@remix-run/router";
+import { useRouteError } from "react-router";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  if (error) {
+    if (error instanceof ErrorResponse) {
+      return (
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            boxShadow: 1,
+            borderRadius: 2,
+            maxWidth: "40vw",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: "50px auto",
+          }}
+        >
+          <Typography variant="h2">Oops!</Typography>
+          <Typography variant="body1">{error.data}</Typography>
+          <Typography variant="body2">{error.statusText}</Typography>
+          <Typography variant="caption">{error.status}</Typography>
+        </Box>
+      );
+    }
+    if (error instanceof Error) {
+      return (
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            boxShadow: 1,
+            borderRadius: 2,
+            maxWidth: "40vw",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: "50px auto",
+          }}
+        >
+          <Typography variant="h2">Oops!</Typography>
+          <Typography variant="body1">{error.message}</Typography>
+        </Box>
+      );
+    }
+  }
+  return (
+    <Box>
+      <Typography variant="h2">Oops!</Typography>
+      <Typography variant="body1">
+        Sorry, an unexpected error has occured.
+      </Typography>
+    </Box>
+  );
+};
+
+export default ErrorPage;
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from "./App.tsx";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./components/ErrorPage.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
