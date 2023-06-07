b0269e0c214ead0df88e7de0187967ff6e2ced01
srabo93 <a.s-dev@web.de>
2023-06-08 00:27:31 +0200

    added todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import Navigation from "./components/Navigation";
-function App() {
+const App = () => {
   return (
     <>
       <Navigation />
     </>
   );
-}
+};
 
 export default App;
+/**
+ * todo: add routes
+ *       -basic routes
+ * todo: configure Material UI
+ *        -theme config
+ * todo: configure firebase and call backenddata / maybe rework how the BaaS works, not the best right now
+ *        -make firebase communicate with app
+ *        -fetch datasets
+ * todo: configure StateManagement
+ *        -workout State and slices for backenddata
+ * todo: complete website styling/components
+ *        -build components with working data/statemanagement
+ */
