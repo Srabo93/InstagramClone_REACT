d2ca9e00f5284978d5266d07a26f700b18133243
srabo93 <a.s-dev@web.de>
2023-06-08 15:55:00 +0200

    configured communication with firebase, fetching of data works

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import Navigation from "./components/Navigation";
 import { ThemeProvider } from "@emotion/react";
 import { themeOptions } from "./themeContext";
+import { db } from "./firebase";
+import { useEffect } from "react";
+import { collection, getDocs, query } from "firebase/firestore";
 
 const App = () => {
+  useEffect(() => {
+    const getAllDocs = async () => {
+      const queryAllDocs = query(collection(db, "Uploads"));
+      const allDocs = await getDocs(queryAllDocs);
+      console.log(allDocs);
+    };
+    getAllDocs();
+  }, []);
   return (
     <ThemeProvider theme={themeOptions}>
       <Navigation />
