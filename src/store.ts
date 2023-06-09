6efd484d00eb29f149c54d39a3df66d2e4b27908
srabo93 <a.s-dev@web.de>
2023-06-10 01:11:01 +0200

    added first state prototype

--- src/store.ts
+++ src/store.ts
@@ -1,54 +1,72 @@
-import { collection, where, query, getDocs } from "firebase/firestore";
+import {
+  collection,
+  where,
+  query,
+  getDocs,
+  DocumentData,
+} from "firebase/firestore";
 import { create } from "zustand";
 import { db } from "./firebase";
 
 interface AppState {
   user: object;
-  posts: object;
+  posts: DocumentData[];
+  isLoading: boolean;
+  error: Error | any;
   getAllPosts: () => Promise<void>;
 }
 
 const useAppStore = create<AppState>((set) => ({
   user: {},
-  posts: {},
+  posts: [],
+  isLoading: false,
+  error: null,
   getAllPosts: async () => {
+    set({ isLoading: true });
+
     const imagesRef = collection(db, "Images");
     const imagesSnapshot = await getDocs(imagesRef);
     const imageIds = imagesSnapshot.docs.map((doc) => doc.id);
 
-    const likesPromises = imageIds.map(async (imageId) => {
-      const likesQuery = query(
-        collection(db, "Likes"),
-        where("imageId", "==", imageId)
+    try {
+      const likesPromises = imageIds.map(async (imageId) => {
+        const likesQuery = query(
+          collection(db, "Likes"),
+          where("imageId", "==", imageId)
+        );
+        const likesSnapshot = await getDocs(likesQuery);
+        return likesSnapshot.docs.map((doc) => doc.data());
+      });
+      const allLikes = await Promise.all(likesPromises);
+
+      const commentsPromises = imageIds.map(async (imageId) => {
+        const commentsQuery = query(
+          collection(db, "Comments"),
+          where("imageId", "==", imageId)
+        );
+        const commentsSnapshot = await getDocs(commentsQuery);
+        return commentsSnapshot.docs.map((doc) => doc.data());
+      });
+      const allComments = await Promise.all(commentsPromises);
+
+      const postsWithLikesAndComments = imagesSnapshot.docs.map(
+        (doc, index) => {
+          const image = doc.data();
+          const likes = allLikes[index];
+          const comments = allComments[index];
+
+          return {
+            ...image,
+            likes,
+            comments,
+          };
+        }
       );
-      const likesSnapshot = await getDocs(likesQuery);
-      return likesSnapshot.docs.map((doc) => doc.data());
-    });
-    const allLikes = await Promise.all(likesPromises);
 
-    const commentsPromises = imageIds.map(async (imageId) => {
-      const commentsQuery = query(
-        collection(db, "Comments"),
-        where("imageId", "==", imageId)
-      );
-      const commentsSnapshot = await getDocs(commentsQuery);
-      return commentsSnapshot.docs.map((doc) => doc.data());
-    });
-    const allComments = await Promise.all(commentsPromises);
-
-    const PostsWithLikesAndComments = imagesSnapshot.docs.map((doc, index) => {
-      const image = doc.data();
-      const likes = allLikes[index];
-      const comments = allComments[index];
-
-      return {
-        ...image,
-        likes,
-        comments,
-      };
-    });
-
-    set({ posts: PostsWithLikesAndComments });
+      set({ posts: postsWithLikesAndComments, isLoading: false });
+    } catch (error) {
+      set({ error: error, isLoading: false });
+    }
   },
 }));
 
