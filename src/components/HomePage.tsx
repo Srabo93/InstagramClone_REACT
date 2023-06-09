8a903b96ff160aef7f7786c59211f903c83addee
srabo93 <a.s-dev@web.de>
2023-06-10 01:11:24 +0200

    added first component prototypes to test the state management

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,22 +1,16 @@
 import { useEffect } from "react";
-import useAppStore from "../store";
 import Post from "./Post";
 
 const HomePage = () => {
-  const fetchPosts = useAppStore((state) => state.getAllPosts);
-
-  const posts = useAppStore((state) => state.posts);
-  console.log(posts);
-
-  useEffect(() => {
-    try {
-      (async () => {
-        await fetchPosts();
-      })();
-    } catch (error) {
-      console.log(error);
-    }
-  }, [fetchPosts]);
+  // useEffect(() => {
+  //   try {
+  //     (async () => {
+  //       await fetchPosts();
+  //     })();
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }, [fetchPosts]);
   return (
     <div style={{ margin: "100px auto" }}>
       <Post />
