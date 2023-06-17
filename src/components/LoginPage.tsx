099d49dd7af842162922a8a4a258703c572b13d2
Srabo93 <a.s-dev@web.de>
2023-06-17 23:39:47 +0200

    added Login Page

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,5 +1,82 @@
+import { getAuth } from "firebase/auth";
+import {
+  Button,
+  Box,
+  Grid,
+  Paper,
+  Typography,
+  Container,
+  Stack,
+} from "@mui/material";
+import { LoginOutlined } from "@mui/icons-material";
+import { signOut } from "firebase/auth";
+import GithubLogin from "./GithubLogin";
+import GoogleLogin from "./GoogleLogin";
+import TwitterLogin from "./TwitterLogin";
+
 const LoginPage = () => {
-  return <div style={{ margin: "100px auto" }}>LoginPage</div>;
+  const auth = getAuth();
+
+  const logOut = async () => {
+    try {
+      await signOut(auth);
+      console.log("logged out");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Container maxWidth="md" sx={{ mt: 10 }}>
+      <Grid container component="main" sx={{ minHeight: "80vh" }}>
+        <Grid
+          item
+          xs={false}
+          sm={false}
+          md={6}
+          component={Paper}
+          elevation={6}
+          sx={{
+            backgroundImage:
+              "url(https://source.unsplash.com/featured/1200x600)",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={5}
+          component={Paper}
+          elevation={6}
+          square
+        >
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <LoginOutlined color="primary" fontSize="large" />
+            <Typography margin={3} component="h1" variant="h5">
+              Login with
+            </Typography>
+            <Stack spacing={3} minWidth="200px">
+              <GoogleLogin />
+              <TwitterLogin />
+              <GithubLogin />
+            </Stack>
+            <Button onClick={logOut}>Logout</Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default LoginPage;
