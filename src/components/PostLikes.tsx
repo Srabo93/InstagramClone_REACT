4296ee3aea975be8fed20618cc8fc5d6d48e4ef9
Srabo93 <a.s-dev@web.de>
2023-06-27 00:18:00 +0200

    added Post Like/Unlike functionality
    User can like or unlike posts

--- src/components/PostLikes.tsx
+++ src/components/PostLikes.tsx
@@ -1,9 +1,17 @@
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import IconButton from "@mui/material/IconButton";
-import { collection, getDocs, query } from "firebase/firestore";
+import {
+  doc,
+  deleteDoc,
+  addDoc,
+  onSnapshot,
+  collection,
+  query,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "../firebase";
 import { Stack, Typography } from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import IconButton from "@mui/material/IconButton";
+import useAppStore from "../store";
 
 type PostLikesProps = {
   postId: string;
@@ -11,42 +19,76 @@ type PostLikesProps = {
 
 export type Like = {
   userId: string;
+  id: string;
 };
 
 const PostLikes = ({ postId }: PostLikesProps) => {
   const [likes, setLikes] = useState<Like[]>([]);
-  const [error, setError] = useState<Error>();
+  const currentUser = useAppStore((state) => state.user);
 
-  useEffect(() => {
-    const fetchLikes = async () => {
-      try {
-        const q = query(collection(db, "Posts", `${postId}`, "Likes"));
-        const likesDocuments: Like[] = [];
+  const addFavorite = async (pId: string) => {
+    await addDoc(collection(db, "Users2", `${currentUser.uid}`, "Favorites"), {
+      postId: pId,
+    });
+    await addDoc(collection(db, "Posts", `${pId}`, "Likes"), {
+      userId: currentUser.uid,
+    });
+  };
 
-        const likesSnapshot = await getDocs(q);
-        likesSnapshot.forEach((like) =>
-          likesDocuments.push(like.data() as Like)
-        );
+  const removeFavorite = async (pId: string) => {
+    const likesId = likes.filter((like) => like.userId === currentUser?.uid);
+    const userRef = doc(
+      db,
+      "Users2",
+      `${currentUser.uid}`,
+      "Favorites",
+      `${pId}`
+    );
+    const likesRef = doc(db, "Posts", `${pId}`, "Likes", likesId[0].id);
+    await deleteDoc(userRef);
+    await deleteDoc(likesRef);
+  };
 
-        if (!ignore) {
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "Posts", `${postId}`, "Likes")),
+        (snapshot) => {
+          const likesDocuments: Like[] = [];
+          snapshot.forEach((like) => {
+            const updatedLike = like.data();
+            updatedLike.id = like.id;
+            likesDocuments.push(updatedLike as Like);
+          });
           setLikes(likesDocuments);
         }
-      } catch (error) {
-        if (error instanceof Error) {
-          setError(error);
-        }
-      }
-    };
+      ),
+    [postId]
+  );
 
-    let ignore = false;
-    fetchLikes();
-    return () => {
-      ignore = true;
-    };
-  }, [postId]);
+  const isLikedByCurrentUser = likes.some(
+    (like) => like.userId === currentUser?.uid
+  );
 
-  if (error) return <div>{error.message}</div>;
+  const likeButton = (
+    <IconButton
+      aria-label="add to favorites"
+      onClick={() => addFavorite(postId)}
+    >
+      <FavoriteIcon />
+    </IconButton>
+  );
 
+  const likedButton = (
+    <IconButton
+      aria-label="add to favorites"
+      onClick={() => removeFavorite(postId)}
+    >
+      <FavoriteIcon color="primary" />
+    </IconButton>
+  );
+
+  const likeIcon = isLikedByCurrentUser ? likedButton : likeButton;
   return (
     <Stack
       direction="row"
@@ -54,9 +96,7 @@ const PostLikes = ({ postId }: PostLikesProps) => {
       alignItems="center"
       spacing={0.5}
     >
-      <IconButton aria-label="add to favorites">
-        <FavoriteIcon />
-      </IconButton>
+      {likeIcon}
       <Typography variant="h5">{likes.length}</Typography>
     </Stack>
   );
