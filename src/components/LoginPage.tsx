1f6b65cb4834b3838aab0198c94ddcec06af9d3c
srabo93 <a.s-dev@web.de>
2023-06-28 15:09:52 +0200

    adjusted LoginPage

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Paper, Typography, Container, Stack } from "@mui/material";
+import { Box, Paper, Typography, Container, Stack } from "@mui/material";
 import { LoginOutlined } from "@mui/icons-material";
 import GithubLogin from "./GithubLogin";
 import GoogleLogin from "./GoogleLogin";
@@ -6,53 +6,30 @@ import TwitterLogin from "./TwitterLogin";
 
 const LoginPage = () => {
   return (
-    <Container maxWidth="md" sx={{ mt: 10 }}>
-      <Grid container component="main" sx={{ minHeight: "80vh" }}>
-        <Grid
-          item
-          xs={false}
-          sm={false}
-          md={6}
-          component={Paper}
-          elevation={6}
-          sx={{
-            backgroundImage:
-              "url(https://source.unsplash.com/featured/1200x600)",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
-        <Grid
-          item
-          xs={12}
-          sm={12}
-          md={5}
-          component={Paper}
-          elevation={6}
-          square
-        >
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <LoginOutlined color="primary" fontSize="large" />
-            <Typography margin={3} component="h1" variant="h5">
-              Login with
-            </Typography>
-            <Stack spacing={3} minWidth="200px">
-              <GoogleLogin />
-              <TwitterLogin />
-              <GithubLogin />
-            </Stack>
-          </Box>
-        </Grid>
-      </Grid>
+    <Container maxWidth="sm" sx={{ mt: 10 }}>
+      <Box
+        component={Paper}
+        sx={{
+          my: 8,
+          mx: 4,
+          py: 2,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          minHeight: "80vh",
+          maxWidth: "50vw",
+        }}
+      >
+        <LoginOutlined color="primary" fontSize="large" />
+        <Typography margin={3} component="h1" variant="h5">
+          Login with
+        </Typography>
+        <Stack spacing={3} minWidth="200px">
+          <GoogleLogin />
+          <TwitterLogin />
+          <GithubLogin />
+        </Stack>
+      </Box>
     </Container>
   );
 };
