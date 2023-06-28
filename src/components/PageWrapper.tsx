10735f04e39d8f02c5576c9cd9058da00cb75736
srabo93 <a.s-dev@web.de>
2023-06-28 16:52:13 +0200

    added page wrapper component

---
+++ src/components/PageWrapper.tsx
@@ -0,0 +1,46 @@
+import { Container } from "@mui/material";
+import { ReactNode } from "react";
+
+const PageWrapper = ({
+  children,
+  column,
+}: {
+  children: ReactNode;
+  column?: boolean;
+}) => {
+  return (
+    <>
+      {column ? (
+        <Container
+          maxWidth="md"
+          sx={{
+            mt: 10,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            minHeight: "80vh",
+          }}
+        >
+          {children}
+        </Container>
+      ) : (
+        <Container
+          maxWidth="md"
+          sx={{
+            mt: 10,
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-evenly",
+            minHeight: "80vh",
+          }}
+        >
+          {children}
+        </Container>
+      )}
+      ;
+    </>
+  );
+};
+export default PageWrapper;
