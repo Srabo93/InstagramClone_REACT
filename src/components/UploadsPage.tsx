344148d5f6a8e5ebd73628aea90569bc56fe40fe
srabo93 <a.s-dev@web.de>
2023-06-30 22:56:29 +0200

    added elements to Uploads

--- src/components/UploadsPage.tsx
+++ src/components/UploadsPage.tsx
@@ -1,5 +1,21 @@
+import PageWrapper from "./PageWrapper";
+import UploadImgGrid from "../ImgGrids/UploadImgGrid";
+import UploadImg from "./UploadImg";
+import { Typography } from "@mui/material";
+
 const UploadsPage = () => {
-  return <div style={{ margin: "100px auto" }}>UploadsPage</div>;
+  return (
+    <PageWrapper column={true}>
+      <Typography variant="h3" color="primary">
+        Your Uploads
+      </Typography>
+      <UploadImg />
+      <Typography variant="h4" color="primary" sx={{ m: 2 }}>
+        View your uploads
+      </Typography>
+      {/* <UploadImgGrid /> */}
+    </PageWrapper>
+  );
 };
 
 export default UploadsPage;
