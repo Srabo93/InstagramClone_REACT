778e094cd7be40afe9c4eb973ddd4d155ff1bace
srabo93 <a.s-dev@web.de>
2023-06-30 23:03:25 +0200

    adjusted uploads site UI
    added a Paper as background

--- src/components/UploadImg.tsx
+++ src/components/UploadImg.tsx
@@ -6,6 +6,7 @@ import {
   TextareaAutosize,
   TextField,
   LinearProgress,
+  Paper,
 } from "@mui/material";
 import { z } from "zod";
 import { styled } from "@mui/material/styles";
@@ -50,134 +51,140 @@ const UploadImg = () => {
   });
 
   return (
-    <Box
-      sx={{
-        m: 5,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      {formData && (
-        <Box width="100%">
-          <LinearProgress variant="determinate" value={progress} />
-        </Box>
-      )}
-      {error && <Alert severity="error">{error}</Alert>}
-      <Formik
-        initialValues={{ image: null, caption: "", title: "" }}
-        validate={(values) => {
-          try {
-            validationSchema.parse(values);
-            return {};
-          } catch (error: any) {
-            return error.formErrors.fieldErrors;
-          }
+    <Paper>
+      <Box
+        sx={{
+          m: 5,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
         }}
-        onSubmit={handleFormSubmit}
       >
-        {({ errors, touched, isSubmitting, setFieldValue }) => (
-          <Form>
-            <Box
-              sx={{
-                m: 5,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-              }}
-            >
-              <CustomLabel htmlFor="image">Image:</CustomLabel>
-              <input
-                id="image"
-                name="image"
-                type="file"
-                style={{ width: "300px" }}
-                onChange={(event) => {
-                  setFieldValue(
-                    "image",
-                    event.currentTarget.files?.[0] || null
-                  );
-                }}
-              />
-              {errors.image && touched.image && (
-                <ErrorMessage
-                  name="image"
-                  component={Alert}
-                  severity="error"
-                  sx={{ mt: 3 }}
-                />
-              )}
-            </Box>
-            <Box
-              sx={{
-                m: 5,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-              }}
-            >
-              <CustomLabel htmlFor="title">Title:</CustomLabel>
-              <TextField
-                type="text"
-                id="title"
-                name="title"
-                size="small"
-                color="primary"
-                placeholder="Enter a title"
-                variant="filled"
-                onChange={(event: ChangeEvent<HTMLTextAreaElement>) => {
-                  setFieldValue("title", event.target?.value);
-                }}
-              />
-              {errors.title && touched.title && (
-                <ErrorMessage name="title" component={Alert} severity="error" />
-              )}
-            </Box>
-            <Box
-              sx={{
-                m: 5,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-              }}
-            >
-              <CustomLabel htmlFor="caption">Caption:</CustomLabel>
-              <TextareaAutosize
-                id="caption"
-                name="caption"
-                placeholder="Enter a caption"
-                minRows={5}
-                style={{ width: "300px" }}
-                onChange={(event) => {
-                  setFieldValue("caption", event.target.value || null);
-                }}
-              />
-              {errors.caption && touched.caption && (
-                <ErrorMessage
-                  name="caption"
-                  component={Alert}
-                  severity="error"
-                  sx={{ mt: 3 }}
-                />
-              )}
-            </Box>
-            <Button
-              type="submit"
-              color="primary"
-              variant="contained"
-              disabled={isSubmitting}
-              sx={{ ml: 5 }}
-            >
-              Upload
-            </Button>
-          </Form>
+        {formData && (
+          <Box width="100%">
+            <LinearProgress variant="determinate" value={progress} />
+          </Box>
         )}
-      </Formik>
-    </Box>
+        {error && <Alert severity="error">{error}</Alert>}
+        <Formik
+          initialValues={{ image: null, caption: "", title: "" }}
+          validate={(values) => {
+            try {
+              validationSchema.parse(values);
+              return {};
+            } catch (error: any) {
+              return error.formErrors.fieldErrors;
+            }
+          }}
+          onSubmit={handleFormSubmit}
+        >
+          {({ errors, touched, isSubmitting, setFieldValue }) => (
+            <Form>
+              <Box
+                sx={{
+                  m: 5,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "start",
+                  flexDirection: "column",
+                }}
+              >
+                <CustomLabel htmlFor="image">Image:</CustomLabel>
+                <input
+                  id="image"
+                  name="image"
+                  type="file"
+                  style={{ width: "300px" }}
+                  onChange={(event) => {
+                    setFieldValue(
+                      "image",
+                      event.currentTarget.files?.[0] || null
+                    );
+                  }}
+                />
+                {errors.image && touched.image && (
+                  <ErrorMessage
+                    name="image"
+                    component={Alert}
+                    severity="error"
+                    sx={{ mt: 3 }}
+                  />
+                )}
+              </Box>
+              <Box
+                sx={{
+                  m: 5,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "start",
+                  flexDirection: "column",
+                }}
+              >
+                <CustomLabel htmlFor="title">Title:</CustomLabel>
+                <TextField
+                  type="text"
+                  id="title"
+                  name="title"
+                  size="small"
+                  color="primary"
+                  placeholder="Enter a title"
+                  variant="filled"
+                  onChange={(event: ChangeEvent<HTMLTextAreaElement>) => {
+                    setFieldValue("title", event.target?.value);
+                  }}
+                />
+                {errors.title && touched.title && (
+                  <ErrorMessage
+                    name="title"
+                    component={Alert}
+                    severity="error"
+                  />
+                )}
+              </Box>
+              <Box
+                sx={{
+                  m: 5,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "start",
+                  flexDirection: "column",
+                }}
+              >
+                <CustomLabel htmlFor="caption">Caption:</CustomLabel>
+                <TextareaAutosize
+                  id="caption"
+                  name="caption"
+                  placeholder="Enter a caption"
+                  minRows={5}
+                  style={{ width: "300px" }}
+                  onChange={(event) => {
+                    setFieldValue("caption", event.target.value || null);
+                  }}
+                />
+                {errors.caption && touched.caption && (
+                  <ErrorMessage
+                    name="caption"
+                    component={Alert}
+                    severity="error"
+                    sx={{ mt: 3 }}
+                  />
+                )}
+              </Box>
+              <Button
+                type="submit"
+                color="primary"
+                variant="contained"
+                disabled={isSubmitting}
+                sx={{ ml: 5 }}
+              >
+                Upload
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </Box>
+    </Paper>
   );
 };
 
