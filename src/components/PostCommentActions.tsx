b53d846346e475399df3d9a558bde96db2e97bbe
srabo93 <a.s-dev@web.de>
2023-07-04 14:36:13 +0200

    adjusted commentactions
    now comment like/dislikes are unique

--- src/components/PostCommentActions.tsx
+++ src/components/PostCommentActions.tsx
@@ -1,4 +1,11 @@
-import { doc, updateDoc, increment, deleteDoc } from "firebase/firestore";
+import {
+  doc,
+  updateDoc,
+  increment,
+  deleteDoc,
+  arrayUnion,
+  onSnapshot,
+} from "firebase/firestore";
 import { db } from "../firebase";
 import { Button, ButtonGroup, Typography } from "@mui/material";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
@@ -6,6 +13,7 @@ import ThumbDownIcon from "@mui/icons-material/ThumbDown";
 import { Comment } from "./PostComments";
 import DeleteIcon from "@mui/icons-material/Delete";
 import useAppStore from "../store";
+import { useEffect, useState } from "react";
 
 const PostCommentActions = ({
   postId,
@@ -14,22 +22,57 @@ const PostCommentActions = ({
   postId: string;
   comment: Comment;
 }) => {
+  const [commentLikes, setCommentLikes] = useState<string[]>([]);
+  const [commentDislikes, setCommentDislikes] = useState<string[]>([]);
+
   const currentUser = useAppStore((state) => state.user);
   const commentRef = doc(db, "Posts", `${postId}`, "Comments", `${comment.id}`);
+  const userRef = doc(db, "Users2", `${currentUser.uid}`);
+
+  useEffect(
+    () =>
+      onSnapshot(userRef, (doc) => {
+        const data = doc.data();
+        if (data) {
+          const likeData: string[] = [...data.commentsLikes];
+          const dislikeData: string[] = [...data.commentsDislikes];
+          setCommentLikes(likeData);
+          setCommentDislikes(dislikeData);
+        }
+      }),
+    []
+  );
 
   const addLikeHandler = async () => {
     await updateDoc(commentRef, {
       likes: increment(1),
     });
+
+    await updateDoc(userRef, {
+      commentsLikes: arrayUnion(comment.id),
+    });
   };
+
   const addDislikeHandler = async () => {
     await updateDoc(commentRef, {
       dislikes: increment(1),
     });
+    await updateDoc(userRef, {
+      commentsDislikes: arrayUnion(comment.id),
+    });
   };
+
   const deleteCommentHandler = async () => {
     await deleteDoc(commentRef);
   };
+
+  const isLiked = commentLikes.some(
+    (likedComment) => likedComment === comment.id
+  );
+
+  const isDisliked = commentDislikes.some(
+    (dislikedComment) => dislikedComment === comment.id
+  );
 
   return (
     <ButtonGroup
@@ -37,13 +80,20 @@ const PostCommentActions = ({
       variant="text"
       aria-label="like / dislike group"
       size="small"
+      disabled={isLiked || isDisliked}
     >
       <Button onClick={addLikeHandler}>
-        <ThumbUpIcon color="secondary" fontSize="small" />
+        <ThumbUpIcon
+          color={isLiked ? "primary" : "secondary"}
+          fontSize="small"
+        />
         <Typography sx={{ px: 1 }}>{comment.likes}</Typography>
       </Button>
       <Button sx={{ px: 1 }} onClick={addDislikeHandler}>
-        <ThumbDownIcon color="secondary" fontSize="small" />
+        <ThumbDownIcon
+          color={isDisliked ? "primary" : "secondary"}
+          fontSize="small"
+        />
         <Typography sx={{ px: 1 }}>{comment.dislikes}</Typography>
       </Button>
       {comment.user.uid === currentUser.uid && (
